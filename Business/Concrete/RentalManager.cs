fb1962e56e9a25dbaa65f5a83f0f0eb21b91e593
Melihcan Şimşek <melihsimsek02@gmail.com>
2023-01-23 13:06:43 +0300

    23/01/2023

--- Business/Concrete/RentalManager.cs
+++ Business/Concrete/RentalManager.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,48 @@ using System.Threading.Tasks;
 
 namespace Business.Concrete
 {
-    public class RentalManager
+    public class RentalManager : IRentalService
     {
+        IRentalDal _rentalDal;
+        public RentalManager(IRentalDal rentalDal)
+        {
+            _rentalDal = rentalDal;
+        }
+        public IResult Add(Rental rental)
+        {
+            var result = _rentalDal.Get(r => r.CarId == rental.CarId);
+            var time = result == null ? -1 : DateTime.Compare((DateTime)result.ReturnDate, rental.RentDate);
+            if (rental.ReturnDate == null || time >= 0)
+            {
+                return new ErrorResult(Message.RentalAddError);
+            }
+            else
+            {
+                _rentalDal.Add(rental);
+                return new SuccessResult(Message.RentalAdded);
+            }
+        }
+
+        public IResult Delete(Rental rental)
+        {
+            _rentalDal.Delete(rental);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<Rental>> GetAll()
+        {
+            return new SuccessDataResult<List<Rental>>(_rentalDal.GetAll());
+        }
+
+        public IDataResult<Rental> GetById(int rentalId)
+        {
+            return new SuccessDataResult<Rental>(_rentalDal.Get(r => r.RentalId == rentalId));
+        }
+
+        public IResult Update(Rental rental)
+        {
+            _rentalDal.Update(rental);
+            return new SuccessResult();
+        }
     }
 }
